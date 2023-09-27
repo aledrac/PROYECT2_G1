6ddd2666480f2020be244193c252dfd4b978471f
unknown <agutierr22jaumebalmes.net>
2023-09-27 16:08:44 +0200

    dispositius

--- Dispositiu.java
+++ Dispositiu.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package proyecto2mobilewolrd;
 
 /**
  *
- * @author Gerard
+ * @author Joan
  */
 public abstract class Dispositiu {
     private String marca;
